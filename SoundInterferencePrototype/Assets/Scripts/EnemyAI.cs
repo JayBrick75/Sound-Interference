bf5e0289394a9b895b9a0277dd9a4475a2b54397
JayBrick75 <128428941+JayBrick75@users.noreply.github.com>
2023-05-05 15:23:18 -0700

    Move proto area once more

--- SoundInterferencePrototype/Assets/Scripts/EnemyAI.cs
+++ SoundInterferencePrototype/Assets/Scripts/EnemyAI.cs
@@ -15,7 +15,7 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
         if (health < 0)
         {
             Destroy(gameObject);
