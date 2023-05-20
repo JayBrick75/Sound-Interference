52a89f5d2dc361526249d1081ca7f5522ce4ece5
JayBrick75 <128428941+JayBrick75@users.noreply.github.com>
2023-05-19 21:15:22 -0700

    enemy

--- SoundInterferencePrototype/Assets/Scripts/EnemyAI.cs
+++ SoundInterferencePrototype/Assets/Scripts/EnemyAI.cs
@@ -1,24 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Script for SOUND INTERFERENCE
 public class EnemyAI : MonoBehaviour
 {
+    // reference for waypoints
+    public List<Transform> points;
+    // int value for indexed list
+    public int nextId;
+    // declare an int to help change nextId
+    private int idChangeValue = 1;
     public float speed;
+    // gets the player
+    public Transform player;
+    // sees the player
+    bool playerSeen = false;
+    // puts the enemy in patrol
+    bool enemyPatrol = true;
     public bool enemyPop = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (enemyPop)
         {
 
         }
+
+        if (enemyPatrol == true)
+        {
+            MoveToNextPoint();
+        }
+
+        CheckForPlayer();
+
+        if (playerSeen == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        void MoveToNextPoint()
+        {
+            // declare and set a transform to the next point
+            Transform goalPoint = points[nextId];
+
+            // flip the enemy via the transform to look at the point's direction
+            // might need to change based off the sprite's natural face
+            if (goalPoint.transform.position.x > transform.position.x)
+            {                                   // 5
+                transform.localScale = new Vector3(-0.9160132f, 0.75976f, 1);
+            }
+            else
+            {                                  // -5
+                transform.localScale = new Vector3(0.9160132f, 0.75976f, 1);
+            }
+
+            // move the enemy towards our point
+            transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
+
+            // check the distance between the enemy and the goalPoint to trigger the next point
+            if (Vector2.Distance(transform.position, goalPoint.position) < 1f)
+            {
+                // check if it's at the end of the line, make the change value -1
+                if (nextId == points.Count - 1)
+                {
+                    idChangeValue = -1;
+                }
+
+                // check if it's at the start of the line, make the change value 1
+                if (nextId == 0)
+                {
+                    idChangeValue = 1;
+                }
+                nextId += idChangeValue;
+            }
+        }
+        void CheckForPlayer()
+        {
+            if (Vector2.Distance(transform.position, player.position) < 4f)
+            {
+                playerSeen = true;
+                enemyPatrol = false;
+            }
+            else
+            {
+                playerSeen = false;
+                enemyPatrol = true;
+            }
+        }
+
     }
 }
